d3910fbb11196c0f29a28b1c6b7fa36053a87e86
Mike <mikeyobviously@gmail.com>
2019-06-18 11:17:33 -0700

    removed stop model, edited imports

--- main_app/models.py
+++ main_app/models.py
@@ -13,19 +13,10 @@ class Profile(models.Model):
 class Tour(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=40, default='Newly Created Tour')
-    numberofstops = models.IntegerField(blank=True)
-    traveldistance = models.FloatField(blank=True)
-    traveltime = models.FloatField(blank=True)
-    regions = models.CharField(max_length=250)
-
-class Stop(models.Model):
-    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
-    region = models.CharField(max_length=50, blank=True)
-    hoursopen = models.CharField(max_length=50, blank=True)
-    phone = models.CharField(max_length=13, blank=True)
+    regions = models.CharField(max_length=250, blank=True)
 
 class Winery(models.Model): 
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100, blank=True, default='')
     desc = models.CharField(max_length=1000, blank=True, default='')
