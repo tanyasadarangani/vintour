0d94a325924b3b82ab86b3bf85886b6aad767c27
Mike <mikeyobviously@gmail.com>
2019-06-18 11:27:33 -0700

    another model change

--- main_app/models.py
+++ main_app/models.py
@@ -10,15 +10,16 @@ class Profile(models.Model):
     location = models.CharField(max_length=50)
     bio = models.TextField()
 
-class Tour(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=40, default='Newly Created Tour')
-    regions = models.CharField(max_length=250, blank=True)
-
 class Winery(models.Model): 
-    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100, blank=True, default='')
     desc = models.CharField(max_length=1000, blank=True, default='')
     price = models.CharField(max_length=50, blank=True, default='')
     rating = models.CharField(max_length=10, blank=True, default='')
+
+class Tour(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, default='Newly Created Tour')
+    regions = models.CharField(max_length=250, blank=True)
+    winery = models.ManyToManyField(Winery)    
+
