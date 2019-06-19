4c491dc6ba03b736d9104ce0226086ca29bd3398
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-06-19 15:11:55 -0700

    list reordering almost complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('tours/<int:tour_id>/', views.tour_detail, name='tour_detail'),
+    path('tours/<int:tour_id>/reorder/', views.stop_reorder, name='stop_reorder'),
     path('accounts/signup/', views.signup, name='signup'),
     path('serp/', views.serp, name='serp'),
     path('profile/', views.profile, name='profile'),
