8b321c0f0d813b1dbe8cf6871bd7b43c263fa732
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-06-18 18:55:31 -0700

    adjustment to model file by someone? git git git

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-# from django.core.paginator import 
-=======
->>>>>>> bdd4e70745606d04996b13ae48855bb000c123b5
 from datetime import date
 
 # Create your models here.
