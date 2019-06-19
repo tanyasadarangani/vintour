347ed824599693f431a94c54d9685d55517408de
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-06-19 10:21:19 -0700

    git

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -32,5 +32,21 @@ $(document).ready(function(){
           dots: true,
       });
 
-      $('.winery-card').draggable()
+      
+        $('div.inline-group').sortable({
+            /*containment: 'parent',
+            zindex: 10, */
+            items: 'div.inline-related',
+            handle: 'h3:first',
+            update: function() {
+                $(this).find('div.inline-related').each(function(i) {
+                    if ($(this).find('input[id$=name]').val()) {
+                        $(this).find('input[id$=order]').val(i+1);
+                    }
+                });
+            }
+        });
+        $('div.inline-related h3').css('cursor', 'move');
+        $('div.inline-related').find('input[id$=order]').parent('div').hide();
+    
 });
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
-<<<<<<< HEAD
-    path('tours/<int:tour_id>', views.tour_detail, name='tour_detail'),
-=======
->>>>>>> e283860acab10ca17fcf40b057fc8d07e9545e5c
     path('serp/', views.serp, name='serp'),
     path('profile/', views.profile, name='profile'),
     path('tours/add/', views.add_winery, name='add_winery'),
